3a0ee9a564b63a4cece39ec3c25e02c74e629136
unknown <rahmawatiaiiii346@gmail.com>
2025-11-04 10:43:51 +0700

    menggunakan private dan namespace

--- LuasLingkaran.php
+++ LuasLingkaran.php
@@ -1,32 +1,43 @@
 <?php
 
+namespace App\Math; //Pasang namespace
 class LuasLingkaran {
     public const phi = 3.14;
-    public int $jari;
-    public function __construct($Isijari = 1){
+    private int $jari;  // variable private
+
+    //constructor
+    public function __construct($Isijari = 1) {
         $this->jari = $Isijari;
     }
-    public function __destruct(){
-        echo "<br>";
-        echo "udah ah cape";
+ 
+    //mengubah nilai jari, karena sebelumnya di private (setter)
+    public function SetJari($nilai){
+        $this->jari = $nilai;
+        return $this->jari;
+
     }
     //Method biasa (non-static)
-    public function tampil($nama = 'ban') {
+    public function tampil($nama = 'ban'){
         $rumus = LuasLingkaran::phi * $this->jari * $this->jari;
         echo "Lingkaran $nama hasilnya adalah:" . $rumus ."<br>";
     
     }
-
+    public function __destruct(){
+        echo "<br>";
+        echo "udah ah cape";
+    }    
     //Method static (bisa dipanggil tanpa objek)
     public static function testing(){
         echo "<br>";
         echo "ini dari static";
         ;
     }
+
 }    
 
 //Pemanggilan 
 $lingkaran = new LuasLingkaran(7);
-$lingkaran->jari = 7;
+$lingkaran->setJari(7);//menjadi setJari karena variable jari di private
+$lingkaran->tampil('kue');
 LuasLingkaran::testing();
 ?>
