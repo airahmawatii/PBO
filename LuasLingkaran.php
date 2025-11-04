70c81bf380cc8e57b792d49a6c87256fdda968a6
unknown <rahmawatiaiiii346@gmail.com>
2025-11-04 10:01:10 +0700

    static dan method

--- LuasLingkaran.php
+++ LuasLingkaran.php
@@ -3,11 +3,26 @@
 class LuasLingkaran {
     public const phi = 3.14;
     public int $jari;
-}
 
+    //Method biasa (non-static)
+    public function tampil($nama = 'ban') {
+        $rumus = self::phi * $this->jari * $this->jari;
+        echo "Lingkaran $nama hasilnya adalah:" . $rumus ."<br>";
+    
+    }
+
+    //Method static (bisa dipanggil tanpa objek)
+    public static function testing($jari){
+        $hasil = self::phi * $jari * $jari;
+        echo "ini hasil dari static :" . $hasil;
+    }
+}    
+
+//Pemanggilan method biasa
 $lingkaran = new LuasLingkaran();
 $lingkaran->jari = 7;
+$lingkaran->tampil('kue');
 
-$rumus = LuasLingkaran::phi * $lingkaran->jari * $lingkaran->jari;
-echo 'hasilnya adalah' . $rumus;
+//pemanggilan method static
+LuasLingkaran::testing(10);
 ?>
